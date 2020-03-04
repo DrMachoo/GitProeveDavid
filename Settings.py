7a8def1077555edb4df44acfc177b78e9b582344
DrMachoo <davitootooop@gmail.com>
2020-03-04 14:05:17 +0100

    yes

---
+++ Settings.py
@@ -0,0 +1,25 @@
+#game settings
+Width = 500
+Height = 500
+PHeight = 60
+PWidth = 40
+Fps = 60
+vel = 10
+x = 50
+y = 50
+Titel = ("MWDJ’s BowRain")
+
+
+
+
+#Farver
+White = (255,255,255)
+Black = (0,0,0)
+Red = (255,0,0)
+Green = (0,255,0)
+Blue = (0,0,255)
+
+
+# Ikke slet please :)
+# greater than >
+# less than <
