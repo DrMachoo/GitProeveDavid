207158ec0af94690386154c696ce829748f8b154
DrMachoo <davitootooop@gmail.com>
2020-03-09 11:25:11 +0100

    yes

--- NewMain.py
+++ NewMain.py
@@ -1,33 +1,53 @@
 import pygame as pg
 from Settings import *
 
+
 pg.init()
 
 screen = pg.display.set_mode((Width, Height))
 
 pg.display.set_caption(Titel)
 
+clock = pg.time.Clock()
 
+def redrawGameWindow():
+    screen.blit(bg, (0,0))
+    man.draw(screen)
+    pg.display.update()
+#MainLoop
+man = Player(300, 410, 64, 64)
 run = True
 while run:
-    pg.time.delay(100)
+    clock.tick(Fps)
+
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+
     keys = pg.key.get_pressed()
 
-    if keys[pg.K_LEFT]:
-        x -= vel
-    if keys[pg.K_RIGHT]:
-        x += vel
-    if keys[pg.K_UP]:
-        y -= vel
-    if keys[pg.K_DOWN]:
-        y += vel
+    if keys[pg.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+    elif keys[pg.K_RIGHT] and man.x < Width - man.Pwidth - man.vel:
+        man.x += man.vel
+        man.right = True
+        man.left = False
+    else:
+        man.right = False
+        man.left = False
+        man.walkCount = 0
 
-    screen.fill(Black)
-    pg.draw.rect(screen, Red, (x, y, PWidth, PHeight))
-    pg.display.update()
+    if keys[pg.K_UP] and man.y > man.vel:
+        man.y -= man.vel
+    if keys[pg.K_DOWN] and man.y < Height - man.Pheight - man.vel:
+        man.y += man.vel
+    if keys[pg.K_ESCAPE]:
+        run = False
+
+
+    redrawGameWindow()
 
 pg.quit()
--- Settings.py
+++ Settings.py
@@ -1,16 +1,39 @@
 #game settings
-Width = 500
-Height = 500
-PHeight = 60
-PWidth = 40
+import pygame as pg
+from Sprites import *
+
+
+class Player(object):
+    def __init__(self, x, y, Pwidth, Pheight):
+        self.x = x
+        self.y = y
+        self.Pwidth = Pwidth
+        self.Pheight = Pheight
+        self.vel = 5
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+
+
+    def draw(self, screen):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            screen.blit(walkLeft[self.walkCount//3], (self.x, self.y) )
+            self.walkCount += 1
+        elif self.right:
+            screen.blit(walkRight[self.walkCount // 3], (self.x, self.y) )
+            self.walkCount += 1
+        else:
+            screen.blit(char, (self.x, self.y) )
+
+
+Width = 1200
+Height = 800
 Fps = 60
 vel = 10
-x = 50
-y = 50
 Titel = ("MWDJ’s BowRain")
-
-
-
 
 #Farver
 White = (255,255,255)
